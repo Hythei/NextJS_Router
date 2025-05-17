f1910914ad8efbe73a2ca6eab984335e134f13c4
HytHei <heikki.hyttinen16@gmail.com>
2025-05-17 11:59:40 +0300

    Ch 16.

--- auth.config.ts
+++ auth.config.ts
@@ -4,4 +4,18 @@ export const authConfig = {
     pages: {
         signIn: '/login',
     },
-} satisfies NextAuthConfig;
+    callbacks: {
+        authorized({ auth, request: { nextUrl }}) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard){
+                if (isLoggedIn) return true;
+                return false; // Sends unauthenticated users to the shadow realm.
+            } else if (isLoggedIn){
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    },
+    providers: [],
+} satisfies NextAuthConfig; 
